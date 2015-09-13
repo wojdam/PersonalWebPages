2c2994f4f4a2fb837254bedf5a77a158242a9710
Martin Vojik <wojdam@email.cz>
2015-09-13 09:08:12 +0200

    Initial commit personal web pages

---
+++ PersonalWebPages/Router.js
@@ -0,0 +1,12 @@
+﻿
+// redirecting function
+var redirectTo = function(page) {
+
+    $("#documentContent").load(page + ".html");
+    //$("#documentContent").empty();
+
+    //$.get(page + ".html", function(data) {
+    //    $("#documentContent").append(data);
+    //});
+
+};
