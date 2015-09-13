dcfa012e303c4aab1636dbb752456c9f523aa3e1
Martin Vojik <wojdam@email.cz>
2015-09-13 14:05:29 +0200

    jQuery added

--- PersonalWebPages/Router.js
+++ PersonalWebPages/Router.js
@@ -1,12 +1,20 @@
 ﻿
+// document is ready DOM constructed.
+$(function () {
+    defaultPage();
+});
+
+// default page for redirect.
+function defaultPage() {
+    this.redirectTo("Home");
+}
+
 // redirecting function
 var redirectTo = function(page) {
 
-    $("#documentContent").load(page + ".html");
-    //$("#documentContent").empty();
-
-    //$.get(page + ".html", function(data) {
-    //    $("#documentContent").append(data);
-    //});
-
+    $("#documentContent").load(page + ".html", function(response, status) {
+        if (status === "error") {
+            defaultPage();
+        }   
+        });
 };
