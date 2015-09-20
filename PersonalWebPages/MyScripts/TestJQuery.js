4a3cba60b86a0ce9b4605a4f27c19551d42841cc
Martin Vojik <wojdam@email.cz>
2015-09-20 11:03:00 +0200

    h1 styling

---
+++ PersonalWebPages/MyScripts/TestJQuery.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+    var height = $(window).height();
+    var heightText = "Window height = " + height;
+    console.log(heightText);
+
+    var width = $(window).width();
+    var widhtText = "Window width = " + width;
+    console.log(widhtText);
+
+    $("#windowHeight").text(height);
+    $("#windowWidth").text(width);
+
+
+});
