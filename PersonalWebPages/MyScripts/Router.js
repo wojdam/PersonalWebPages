2b0c3176822c95c581c59124240e97ea8e748438
Martin Vojik <wojdam@email.cz>
2015-09-20 15:20:22 +0200

    New menu based on Bootstrap

--- PersonalWebPages/MyScripts/Router.js
+++ PersonalWebPages/MyScripts/Router.js
@@ -6,15 +6,19 @@ $(function () {
 
 // default page for redirect.
 function defaultPage() {
-    this.redirectTo("Home");
+    this.redirectTo("Home", "navHome");
 }
 
 // redirecting function
-var redirectTo = function(page) {
+var redirectTo = function(page, selItem) {
 
     $("#documentContent").load(page + ".html", function(response, status) {
         if (status === "error") {
             defaultPage();
-        }   
-        });
+        }
+
+        $("#menuRoot").children().removeClass("active");
+
+        $("#" + selItem).addClass("active");
+    });
 };
